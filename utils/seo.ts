c5690bac4632937c34b3ca6ea8780e4b6e4c25ad
doinkythederp <doinkythederp@icloud.com>
2023-10-26 18:29:29 -0700

    Add branding

--- utils/seo.ts
+++ utils/seo.ts
@@ -6,7 +6,7 @@ import type { ManifestOptions } from "vite-plugin-pwa";
  */
 export const seoConfig = {
     baseURL: "https://3636.live", // Change this to your production URL.
-    description: "Stream live matches from 3636 Generals Robotics", // Change this to be your website's description.
+    description: "Stream live matches from FRC 3636 - Generals Robotics", // Change this to be your website's description.
     type: "website",
     image: {
         url: "/android-chrome-512x512.png",
@@ -14,7 +14,7 @@ export const seoConfig = {
         width: 512,
         height: 512,
     },
-    siteName: "3636.live - Generals Robotics", // Change this to your website's name,
+    siteName: "3636.live", // Change this to your website's name,
 };
 export const manifest: Partial<ManifestOptions> = {
     name: seoConfig.siteName, // Change this to your website's name.
@@ -24,21 +24,28 @@ export const manifest: Partial<ManifestOptions> = {
     background_color: "#000000", // Change this to your background color.
     icons: [
         {
-            src: "/maskable-640x640.png",
-            sizes: "640x640",
+            src: "/maskable-2243x2243.png",
+            sizes: "2243x2243",
             type: "image/png",
-            purpose: "maskable",
-        },
-        {
-            src: "/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            purpose: "any maskable",
         },
         {
             src: "/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any",
+            purpose: "any maskable",
+        },
+        {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+        },
+        {
+            src: "/assets/logo.png",
+            sizes: "1178x1458",
+            type: "image/png",
+            purpose: "monochrome",
         },
     ],
 };
