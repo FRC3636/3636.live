4e760002ea52d7f4e54ccc3549f03cb952db8d3c
doinkythederp <doinkythederp@icloud.com>
2024-01-08 00:03:08 -0800

    feat: add sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,21 @@ import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry()],
-  site: "https://3636.live",
-  vite: {
-    plugins: [VitePWA({
-      manifest
-    })]
-  }
+    integrations: [
+        sentry({
+            dsn: "https://d9846d5e2c53aec80163141f77a9cabb@o1106499.ingest.sentry.io/4506118179586048",
+            sourceMapsUploadOptions: {
+                project: "3636-live",
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+            },
+        }),
+    ],
+    site: "https://3636.live",
+    vite: {
+        plugins: [
+            VitePWA({
+                manifest,
+            }),
+        ],
+    },
 });
