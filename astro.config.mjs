968fed48f08eccc57b0df9a8924903e4d90b1b62
doinkythederp <doinkythederp@icloud.com>
2024-01-07 23:59:07 -0800

    feat: use new branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@ import { defineConfig } from "astro/config";
 import { VitePWA } from "vite-plugin-pwa";
 import { manifest } from "./utils/seo";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [],
-    site: "https://3636.live",
-    vite: {
-        plugins: [
-            VitePWA({
-                manifest,
-            }),
-        ],
-    },
+  integrations: [sentry()],
+  site: "https://3636.live",
+  vite: {
+    plugins: [VitePWA({
+      manifest
+    })]
+  }
 });
