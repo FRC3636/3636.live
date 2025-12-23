6c2314a230e1ea830bb852aceec64bf8260fc4bc
Eve <zezetta@icloud.com>
2025-12-23 17:31:33 -0500

    update deps and remove sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,8 @@ import { defineConfig } from "astro/config";
 import { VitePWA } from "vite-plugin-pwa";
 import { manifest } from "./utils/seo";
 
-import sentry from "@sentry/astro";
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        sentry({
-            dsn: "https://d9846d5e2c53aec80163141f77a9cabb@o1106499.ingest.sentry.io/4506118179586048",
-            sourceMapsUploadOptions: {
-                project: "3636-live",
-                authToken: process.env.SENTRY_AUTH_TOKEN,
-            },
-        }),
-    ],
     site: "https://3636.live",
     vite: {
         plugins: [
